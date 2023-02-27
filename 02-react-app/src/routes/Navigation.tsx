da851183ff89196a72f9845abcee20821a19e5c7
CesarArellano <raywayday@gmail.com>
2023-02-27 10:53:08 -0600

    Know about lazyload with suspense component

---
+++ 02-react-app/src/routes/Navigation.tsx
@@ -0,0 +1,42 @@
+
+import { Suspense } from 'react';
+import { BrowserRouter, Routes, Route, NavLink, Navigate } from 'react-router-dom'
+
+import logo from '../logo.svg';
+import { routes } from './routes';
+
+export const Navigation = () => {
+  return (
+    <Suspense fallback={ <span> Loading </span> } >
+      <BrowserRouter>
+        <div className="main-layout">
+          <nav>
+            <img src={ logo } alt="React Logo"></img>
+            <ul>
+              {
+                routes.map(({ to, name }) =>(
+                  <li key={ name }>
+                    <NavLink 
+                      to={ to }
+                      className={({ isActive }) => isActive ? 'nav-active' : '' }
+                    >
+                      { name }
+                    </NavLink>
+                  </li>
+                ))
+              }
+            </ul>
+          </nav>
+          <Routes>
+            {
+              routes.map(({ path, Component }) =>(
+                <Route key={ path } path={ path } element={ <Component /> } />
+              ))
+            }
+            <Route path='/*' element={ <Navigate to={ routes[0].to } replace /> } />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </Suspense>
+  )
+}
