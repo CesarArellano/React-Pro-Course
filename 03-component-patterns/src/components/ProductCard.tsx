0aff659e6ae34cba740cf36a1d572dad40c12b30
CesarArellano <raywayday@gmail.com>
2023-04-03 22:25:18 -0600

    Change interfaces in 03-component-patterns

--- 03-component-patterns/src/components/ProductCard.tsx
+++ 03-component-patterns/src/components/ProductCard.tsx
@@ -7,13 +7,13 @@ import styles from '../styles/styles.module.css';
 export const ProductContext = createContext({} as ProductContextProps);
 const { Provider } = ProductContext;
 
-export interface ProductCardProps {
+export interface Props {
   product: Product;
   children: ReactElement | ReactElement[],
   className?: string
 }
 
-export const ProductCard = ({ children, product, className }: ProductCardProps) => {
+export const ProductCard = ({ children, product, className }: Props) => {
   const { counter, increaseBy } = useProduct();
   
   return (
