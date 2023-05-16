0f824f28128140e76f9b828898c4543f396fb729
Cesar Arellano <raywayday@gmail.com>
2023-05-15 20:05:57 -0600

    Know about getFieldProps of useFormik and applied to FormikYupPage

--- 05-formik/src/03-forms/pages/FormikYupPage.tsx
+++ 05-formik/src/03-forms/pages/FormikYupPage.tsx
@@ -1,18 +1,11 @@
-import { FormikErrors, useFormik } from 'formik';
+import { useFormik } from 'formik';
 import * as Yup from 'yup';
 
 import '../styles/styles.css';
 
-interface FormValues {
-  firstName: string;
-  lastName: string;
-  email: string;
-}
-
 export const FormikYupPage = () => {
-
   
-  const { handleChange, handleSubmit, values, errors, touched, handleBlur } = useFormik({
+  const { handleSubmit, errors, touched, getFieldProps } = useFormik({
     initialValues: {
       firstName: '',
       lastName: '',
@@ -39,31 +32,13 @@ export const FormikYupPage = () => {
       <h1>Formik Yup Page</h1>
       <form noValidate onSubmit={ handleSubmit }>
         <label htmlFor="firstName">First Name</label>
-        <input 
-          type="text" 
-          name='firstName'
-          onBlur={ handleBlur }
-          onChange={ handleChange }
-          value={ values.firstName }
-        />
+        <input type="text" { ...getFieldProps('firstName') } />
         { touched.firstName && errors.firstName && <span>{ errors.firstName }</span> }
         <label htmlFor="lastName">Last Name</label>
-        <input 
-          type="text" 
-          name='lastName'
-          onBlur={ handleBlur }
-          onChange={ handleChange }
-          value={ values.lastName }
-        />
+        <input type="text" { ...getFieldProps('lastName') } />
         { touched.lastName && errors.lastName && <span>{ errors.lastName }</span> }
         <label htmlFor="email">Email Address</label>
-        <input 
-          type="email" 
-          name='email'
-          onBlur={ handleBlur }
-          onChange={ handleChange }
-          value={ values.email }
-        />
+        <input type="email" { ...getFieldProps('email') } />
         { touched.email && errors.email && <span>{ errors.email }</span> }
         <button type='submit'>Submit</button>
       </form>
