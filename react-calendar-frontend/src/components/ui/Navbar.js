f4f512cba99481c9a2a416dcc3d5857604e356d0
Cesar Arellano <raywayday@gmail.com>
2023-05-25 17:00:31 -0600

    Improve sw-template and add react-detect-offline package

--- react-calendar-frontend/src/components/ui/Navbar.js
+++ react-calendar-frontend/src/components/ui/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { startLogout } from '../../actions/auth';
+import { Online, Offline } from 'react-detect-offline';
 
 export const Navbar = () => {
   const dispatch = useDispatch();
@@ -9,11 +10,14 @@ export const Navbar = () => {
     dispatch( startLogout() );
   }
 
+
   return (
     <div className="navbar navbar-dark bg-dark mb-4">
       <span className="navbar-brand">
         { name }
       </span>
+      <Online><span className='text-success'>Online</span></Online>
+      <Offline><span className='text-warning'>Offline- Requests will be saved</span></Offline>
       <button onClick={ handleLogout } className="btn btn-outline-danger">
         <span className="textButton">Salir</span>
         <i className="fas fa-sign-out-alt"></i>
--- react-calendar-frontend/src/sw-template.js
+++ react-calendar-frontend/src/sw-template.js
@@ -2,4 +2,63 @@ importScripts(
   'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
 );
 
-workbox.precaching.precacheAndRoute( self.__WB_MANIFEST )
+workbox.loadModule('workbox-background-sync');
+
+workbox.precaching.precacheAndRoute( self.__WB_MANIFEST );
+
+const { registerRoute } = workbox.routing;
+const { CacheFirst, NetworkFirst, NetworkOnly } = workbox.strategies;
+const { BackgroundSyncPlugin } = workbox.backgroundSync;
+
+const networkFirstPaths = [
+  '/api/auth/renew',
+  '/api/events',
+]
+
+const cacheFirstPaths = [
+  'https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css',
+]
+
+
+registerRoute(
+  ({ _, url }) => networkFirstPaths.includes( url.pathname ),
+  new NetworkFirst()
+);
+
+registerRoute(
+  ({ _, url }) => cacheFirstPaths.includes( url.pathname ),
+  new CacheFirst()
+);
+
+
+// Offline Posts
+
+const bgSyncPlugin = new BackgroundSyncPlugin('offline-posts', {
+  maxRetentionTime: 24 * 60, // Retry for max of 24 Hours (specified in minutes)
+});
+
+registerRoute(
+  new RegExp('http://localhost:4000/api/events'),
+  new NetworkOnly({
+    plugins: [ bgSyncPlugin ]
+  }),
+  'POST'
+)
+
+registerRoute(
+  new RegExp('http://localhost:4000/api/events'),
+  new NetworkOnly({
+    plugins: [ bgSyncPlugin ]
+  }),
+  'PUT'
+)
+
+registerRoute(
+  new RegExp('http://localhost:4000/api/events'),
+  new NetworkOnly({
+    plugins: [ bgSyncPlugin ]
+  }),
+  'DELETE'
+)
+
