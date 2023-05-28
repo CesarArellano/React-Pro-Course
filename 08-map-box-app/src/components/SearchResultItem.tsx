5ebd1b10b495b0cc33f1da1541fee759cf1df784
Cesar Arellano <raywayday@gmail.com>
2023-05-27 23:50:04 -0600

    Add activeId and onPlacedCliked function to SearchResultItem

--- 08-map-box-app/src/components/SearchResultItem.tsx
+++ 08-map-box-app/src/components/SearchResultItem.tsx
@@ -1,22 +1,26 @@
 import { Feature } from "../interfaces/places"
-
 interface Props {
-  place: Feature
+  place: Feature,
+  activeId: string,
+  onPlacedCliked: ( place: Feature ) => void
 }
 
-export const SearchResultItem = ({ place }: Props) => {
+export const SearchResultItem = ({ place, activeId, onPlacedCliked }: Props) => {
+  
   return (
-    <li className="list-group-item list-group-item-action">
+    <li 
+      className={ `${ activeId === place.id && 'active' } list-group-item list-group-item-action pointer` }
+      onClick={ () => onPlacedCliked(place) }
+    >
       <h6>{ place.text_es }</h6>
-      <p 
-        className="text-muted"
+      <p
         style={{
           fontSize: '12px'
         }}
       >
         { place.place_name }
       </p>
-      <button className="btn btn-outline-primary btn-sm">
+      <button className={ `btn btn-sm ${ activeId === place.id ? 'btn-outline-light' : 'btn-outline-primary' }` }>
         Direcciones
       </button>
     </li>
--- 08-map-box-app/src/components/SearchResults.tsx
+++ 08-map-box-app/src/components/SearchResults.tsx
@@ -1,10 +1,25 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { PlacesContext } from '../context';
 import { SearchResultItem } from './SearchResultItem';
 import { LoadingPlaces } from '.';
+import { MapContext } from '../context/map/MapContext';
+import { Feature } from '../interfaces/places';
 
 export const SearchResults = () => {
   const { places, isLoadingPlaces } = useContext(PlacesContext);
+
+  const [activeId, setActiveId] = useState('');
+  const { map } = useContext(MapContext);
+
+  const onPlacedCliked = ( place: Feature ) => {
+    const [ lng, lat ] = place.center;
+    map?.flyTo({
+      zoom: 14,
+      center: [ lng, lat ]
+    })
+    setActiveId(place.id)
+  }
+
 
   if( isLoadingPlaces) {
     return <LoadingPlaces />
@@ -17,7 +32,14 @@ export const SearchResults = () => {
   return (
     <ul className="list-group mt-3">
       {
-        places.map((place) => <SearchResultItem key={ place.id } place={ place }/>)
+        places.map((place) => (
+          <SearchResultItem 
+            key={ place.id }
+            place={ place }
+            activeId={ activeId }
+            onPlacedCliked={ onPlacedCliked }
+          />
+        ))
       }
       
     </ul>
